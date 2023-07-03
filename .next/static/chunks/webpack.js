2c66e7b44e0ea2c528e3dabfc2a1eb0c9226bf1c
Stuart Wayland <stuartwayland@ucsb.edu>
2023-07-02 18:13:08 -0700

    minor update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "606e7a2f743fcc01"; }
+/******/ 		__webpack_require__.h = function() { return "5a1d20cad57306ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "4ac599b75ee9791e"; }
+/******/ 	__webpack_require__.h = function() { return "4ef070f2c8ef7f6f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
