9ee478e909566836de6142f55ad46f7d5dcbb744
Stuart Wayland <stuartwayland@ucsb.edu>
2023-08-03 09:21:34 -0700

    Adding same size grid

--- pages/team.js
+++ pages/team.js
@@ -13,42 +13,88 @@ const Team = () => {
       </Head>
   
 
-    <div class="min-h-screen flex flex-col items-center justify-center">  
-      <div class="flex flex-row items-center ">    
-      <div>
-        <Card name="Jeremy Wayland" 
+      <div class="min-h-screen flex flex-col justify-center items-center">
+  <div class="grid grid-cols-2 gap-4">
+    <div>
+      <Card
+        name="Jeremy Wayland"
         image="/place_holder.png"
-        description="PhD Student at Helmholtz Munich."
-         githubUrl="https://github.com/jeremy-wayland"
-         linkedInUrl="https://www.linkedin.com/in/jeremy-wayland/"
-         websiteUrl="https://jeremy-wayland.me/"
+        description="PhD Student - Helmholtz Munich."
+        githubUrl="https://github.com/jeremy-wayland"
+        linkedInUrl="https://www.linkedin.com/in/jeremy-wayland/"
+        websiteUrl="https://jeremy-wayland.me/"
         facebookUrl="https://www.facebook.com/jeremy.wayland.92"
-        >
-    </Card>
+      ></Card>
     </div>
-    <div class="ml-3 sm:ml-2 md:ml-3 lg:ml-4">
-    <Card name="Sidney Gathrid"
+    <div>
+      <Card
+        name="Sidney Gathrid"
         image="/place_holder.png"
-        description="Env technology solutions engineer."
+        description="Employed - Solutions Engineer."
         githubUrl="https://github.com/sgathrid"
         linkedInUrl="https://www.linkedin.com/in/sidney-gathrid-783a3b229/"
-       facebookUrl="https://www.facebook.com/sidney.gathrid.1"
-        >
-    </Card>
+        facebookUrl="https://www.facebook.com/sidney.gathrid.1"
+      ></Card>
     </div>
+  </div>
+  <div>
+  <div class= "mt-4 sm:mt-3 md:mt-4 lg:mt-5">
+  
+      <Card
+        name="Stuart Wayland"
+        image="/place_holder.png"
+        description="Masters Student - UC Santa Cruz."
+        githubUrl="https://github.com/stuartwayland"
+        linkedInUrl="https://www.linkedin.com/in/stuart-wayland-96b621253/"
+        websiteUrl="https://stuartwayland.github.io/"
+        facebookUrl="https://www.facebook.com/stuart.wayland.7/"
+        imageClasses="mx-auto"
+      ></Card>
     </div>
-
-    <div class="mt-3 sm: mt-2 md:mt-3 lg:mt-4 mb-4 sm:mb-3 md:mb-4 lg:mb-5">
-    <Card name="Stuart Wayland"
-          image="/place_holder.png"
-          description="Masters Student at UC Santa Cruz."
-          githubUrl="https://github.com/stuartwayland"
-         linkedInUrl="https://www.linkedin.com/in/stuart-wayland-96b621253/"
-         websiteUrl="https://stuartwayland.github.io/"
-        facebookUrl="https://www.facebook.com/stuart.wayland.7/">
-    </Card>
-    </div>  
+  </div>
 </div>
+
+
+  {/* <div class="flex flex-col justify-center items-center">
+  <div class="flex grid place-items-center">
+  <div class="grid grid-cols-2 gap-4">
+    <div>
+      <Card
+        name="Jeremy Wayland"
+        image="/place_holder.png"
+        description="PhD Student - Helmholtz Munich."
+        githubUrl="https://github.com/jeremy-wayland"
+        linkedInUrl="https://www.linkedin.com/in/jeremy-wayland/"
+        websiteUrl="https://jeremy-wayland.me/"
+        facebookUrl="https://www.facebook.com/jeremy.wayland.92"
+      ></Card>
+    </div>
+    <div>
+      <Card
+        name="Sidney Gathrid"
+        image="/place_holder.png"
+        description="Employed - Emrgy."
+        githubUrl="https://github.com/sgathrid"
+        linkedInUrl="https://www.linkedin.com/in/sidney-gathrid-783a3b229/"
+        facebookUrl="https://www.facebook.com/sidney.gathrid.1"
+      ></Card>
+    </div>
+    </div>
+    <div>
+      <Card
+        name="Stuart Wayland"
+        image="/place_holder.png"
+        description="Masters Student - UC Santa Cruz."
+        githubUrl="https://github.com/stuartwayland"
+        linkedInUrl="https://www.linkedin.com/in/stuart-wayland-96b621253/"
+        websiteUrl="https://stuartwayland.github.io/"
+        facebookUrl="https://www.facebook.com/stuart.wayland.7/"
+        imageClasses="mx-auto"
+      ></Card>
+    </div>
+    </div>
+    </div> */}
+
 </main>
     );
   };
